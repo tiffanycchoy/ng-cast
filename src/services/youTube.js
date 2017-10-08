33bc930c8dcc8bd703048dae1e1467a4cbc0c51a
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-08 16:57:45 -0700

    create a resusable API helper: services/youTube

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,25 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function($http){
+
+  this.searchYouTube= function(searchTerm, callback) {
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        part:'snippet',
+        q: searchTerm,
+        maxResults: 5,
+        key: window.YOUTUBE_API_KEY,
+        videoEmbeddable: true,
+        type: 'video'
+      }
+    }).then(function successCallback(response) {
+      console.log('successfully retrieved data from youtube ', response.data.items);
+        callback(response.data.items);
+
+    }, function errorCallback(response) {
+      console.log('error retrieving data from youtube');
+    });
+  }
+
 });
