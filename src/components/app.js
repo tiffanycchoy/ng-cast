cb08b67368fae36745afb83cabe3519f137585cd
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-07 20:46:48 -0700

    create a dynamic video list component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,9 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
-});
+  templateUrl: '../src/templates/app.html',
+  controller: function() {
+    this.videos = window.exampleVideoData;
+    this.currentVideo = window.exampleVideoData[0];
+  }
+})
--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,4 +1,9 @@
 angular.module('video-player')
 .component('videoListEntry', {
   // TODO
-});
+  bindings: {
+    videos: '<',
+    video: '<'
+  },
+  templateUrl: '../src/templates/videoListEntry.html'
+})
