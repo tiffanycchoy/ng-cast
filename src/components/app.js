bea34bcf8c9433df9927e6a37e47c664d36bf64e
tiffanycchoy <tiffany.c.choy@gmail.com>
2017-10-08 17:00:31 -0700

    implement search component

--- src/components/app.js
+++ src/components/app.js
@@ -2,14 +2,29 @@ angular.module('video-player')
 
 .component('app', {
   templateUrl: '../src/templates/app.html',
-  controller: function() {
+  controller: function(youTube) {
     this.videos = window.exampleVideoData;
-    this.currentVideo = window.exampleVideoData[0];
+    this.currentVideo = this.videos[0];
+    this.searchTerm = '';
 
     this.handleVideoEntryClick = function(selectedVideo) {
       console.log('this is ', this);
       console.log("the selectedVideo is ", selectedVideo);
       this.currentVideo = selectedVideo;
     }.bind(this);
+
+    this.handleSearchTerm = function(searchQuery) {
+      console.log('search term is ', searchQuery);
+      this.searchTerm = searchQuery;
+    }.bind(this);
+
+    this.handleSearchButton = function() {
+      var context = this;
+      youTube.searchYouTube(this.searchTerm, function(videos) {
+        context.videos = videos;
+        context.currentVideo = videos[0];
+      })
+    }.bind(this);
+
   }
 })
--- src/components/search.js
+++ src/components/search.js
@@ -2,4 +2,9 @@ angular.module('video-player')
 
 .component('search', {
   // TODO
+  bindings: {
+    handleSearchTerm: "<",
+    handleSearchButton: "<"
+  },
+  templateUrl: '../src/templates/search.html'
 });
